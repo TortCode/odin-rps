6e485f95225c256172dfaba4a7306ae1d78260ae
Teerth (Macbook) <Teerth Patel>
2024-11-24 19:13:26 -0600

    Implement computer and human choice of hand

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+console.log("Hello World");
+
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * 3);
+    switch (index) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    while (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
+        choice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors.").toLowerCase();
+    }
+    return choice;
+}
